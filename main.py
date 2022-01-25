408c2b2d2bf4192b56477af3e6fb72e060c46034
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2022-01-25 21:56:51 +0100

    Get column names after applying the column-transformer

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 
 dt = pd.read_csv("./datasets/train.csv")
-print(dt.columns)
 
 categorical_features = ['Sex', 'Embarked', 'Pclass']
 numerical_features = ['Age', 'SibSp', 'Fare', 'Parch']
+
+SimpleImputer.get_feature_names_out = (lambda self, names=None: self.feature_names_in_)
 
 numerical_pipeline = Pipeline(steps=[
     ('numerical_impute', SimpleImputer(missing_values=np.nan, strategy="median")),
@@ -26,5 +27,7 @@ column_transformer = ColumnTransformer(transformers=[
     ('numerical_pipeline', numerical_pipeline, numerical_features)], remainder="drop")
 
 x = column_transformer.fit_transform(dt)
+
+print("Column names: ",column_transformer.get_feature_names_out())
 
 print(x)
