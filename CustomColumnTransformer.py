4eece55f153fcb693c749c8b93ca4d0baf1c1c81
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2022-01-26 21:46:56 +0100

    Add CustomColumnTransformer

--- CustomColumnTransformer.py
+++ CustomColumnTransformer.py
@@ -1,10 +1,37 @@
+import numpy as np
+
+
 class CustomColumnTransformer:
 
     def __init__(self, func):
         self.func = func
 
-    def fit(self):
+    def fit(self, X, y=None, **fit_params):
         return self
 
-    def transform(self,X):
-        return self.func(X)
+    def transform(self, input_df, **transform_params):
+        return self.func(input_df)
+
+    # if cabin nr is C85 then rename it to C
+    @staticmethod
+    def rename_cabin(X):
+        X['Cabin'] = X['Cabin'].map(lambda x: x[0] if not (x is np.nan) else "UNKNOWN")
+        return X
+
+    @staticmethod
+    def impute_cabin(X):
+        X['Cabin'] = X['Cabin'].map(
+            lambda x: CustomColumnTransformer.get_deck_nr(x) if x == "UNKNOWN" or x == "T" else x)
+        return X
+
+    @staticmethod
+    def get_deck_nr(pclass):
+        if pclass == 1:
+            return np.random.choice(['A', 'B', 'C', 'D', 'E'])
+        elif pclass == 2:
+            return np.random.choice(['D', 'E'])
+        else:
+            return np.random.choice(['F', 'G'])
+
+    def get_feature_names_out(self, X):
+        return ['Cabin']
