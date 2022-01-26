7dc930bd390942f1f943a4e8f78edaa58bd191cb
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2022-01-26 23:39:55 +0100

    Create a neural-network to predict survivors

---
+++ titanic_neural_network.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from Dataset import Dataset
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Sequential
+
+dt = Dataset()
+x_train, x_test, y_train, y_test = dt.get_train_test_dataset()
+
+model = Sequential()
+model.add(Input(shape=x_train.shape[1]))
+model.add(Dense(35, activation='relu'))
+model.add(Dense(35, activation='relu'))
+model.add(Dense(35, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# model.summary()
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+model.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), epochs=10)
+
+predictions = tf.round(model.predict(dt.test_dt).flatten())
+dt = pd.read_csv('./datasets/test.csv')
+dt = dt.drop(['Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis=1)
+dt['Survived'] = predictions
+print(dt)
